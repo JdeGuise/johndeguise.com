896477c9f367c0457e2bd91263ff705d3c1182cf
John deGuise <jrd120893@gmail.com>
2017-07-10 18:17:10 -0400

    Initial commits; adds setup scripts for project

---
+++ setup_scripts/permissions.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+chmod -R 755 `cat folders.txt`
+chmod -R 644 `cat files.txt`
---
+++ setup_scripts/project_setup.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo "Removing existing project from public_html"
+rm -rf ~/public_html/*
+echo "Done removing project."
+
+sleep 3
+
+cd ~/project_repo/johndeguise.com
+
+echo "Pulling down new updates"
+
+git pull origin prod
+
+echo "Done pulling new updates"
+
+echo "Copying project structure to live folder"
+
+cp -r ~/project_repo/johndeguise.com/public_html/* ~/public_html/
+
+echo "Done copying project structure to live public_html folder"
+
+cd ~/scripts
+
+echo "Running permissions script"
+
+bash permissions.sh
+
+echo "Done running permissions script. Check www.johndeguise.com to verify changes."
