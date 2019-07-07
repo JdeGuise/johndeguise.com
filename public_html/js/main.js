c41bd9ac8804957a0bba93462d68a70e771fa59c
John deGuise <jrd120893@gmail.com>
2019-07-07 00:44:51 -0600

    Refactors to style; data updates; refactors to javascript; refactors to html structure

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,30 +1,58 @@
-$(".aboutnav").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".about").offset().top},
-        'slow');
-});
-$(".edunav").click(function() {
-	$('html,body').animate({
-		scrollTop: $(".education").offset().top},
-		'slow');
-});
-$(".worknav").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".work").offset().top},
-        'slow');
-});
-$(".projectsnav").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".projects").offset().top},
-        'slow');
-});
-$(".volunteernav").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".volunteer").offset().top},
-        'slow');
-});
-$(".awardsnav").click(function() {
-    $('html,body').animate({
-        scrollTop: $(".awards").offset().top},
-        'slow');
-});
+function loadNav() {
+    const request = new XMLHttpRequest();
+    request.open('GET', 'nav.html', true);
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            document.querySelector('#nav_wrapper').innerHTML = request.responseText;
+        }
+    };
+    request.send();
+}
+
+function loadFooter() {
+    const request = new XMLHttpRequest();
+    request.open('GET', 'footer.html', true);
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            document.querySelector('#footer_wrapper').innerHTML = request.responseText;
+        }
+    };
+    request.send();
+}
+
+function handleClickEvents() {
+    $(".aboutnav").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".about").offset().top},
+            'slow');
+    });
+    $(".edunav").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".education").offset().top},
+            'slow');
+    });
+    $(".worknav").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".work").offset().top},
+            'slow');
+    });
+    $(".projectsnav").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".projects").offset().top},
+            'slow');
+    });
+    $(".volunteernav").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".volunteer").offset().top},
+            'slow');
+    });
+    $(".awardsnav").click(function() {
+        $('html,body').animate({
+            scrollTop: $(".awards").offset().top},
+            'slow');
+    });
+}
+
+loadNav();
+loadFooter();
+handleClickEvents();
