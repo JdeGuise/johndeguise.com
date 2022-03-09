6b3cb975acddb4aa3f0a815c02ce0e1f9d337f10
John deGuise <john.r.deguise@gmail.com>
2022-03-08 21:43:18 -0500

    support for multiple pages -- adding linking structure and routing for travel blog, projects, and about pages

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/blog', (req, res) => res.render('pages/blog'))
+  .get('/projects', (req, res) => res.render('pages/projects'))
+  .get('/about', (req, res) => res.render('pages/about'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
