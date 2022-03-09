06f59d8a09a6c0219c5cfac37ab35d475b7fb415
John deGuise <john.r.deguise@gmail.com>
2022-03-08 22:08:21 -0500

    initial commit of maintenance page

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/blog', (req, res) => res.render('pages/blog'))
+  .get('/projects', (req, res) => res.render('pages/projects'))
+  .get('/about', (req, res) => res.render('pages/about'))
+  .get('/maintenance', (req, res) => res.render('pages/maintenance'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
