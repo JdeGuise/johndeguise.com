0685e8016695c4132db746e78c54c8300d3ace28
John deGuise <john.r.deguise@gmail.com>
2024-06-07 20:43:39 +0200

    Cleaning up footer css

--- vite-project/src/components/Footer.jsx
+++ vite-project/src/components/Footer.jsx
@@ -2,17 +2,20 @@ import './Footer.css'
 
 function Footer() {
   return (
-    <div class="footer-contents">
-      <div class="footer-contents-social">
-        <a href="https://www.linkedin.com/in/john-r-deguise"><img src="linkedin.svg" alt=""/></a>
-        <a href="https://www.github.com/JdeGuise"><img src="github.svg" alt=""/></a>
-        <a href="mailto:john@johndeguise.com"><img src="email.svg" alt=""/></a>
-      </div>
+    <div class="footer">
+      <div class="footer-contents">
+        <div class="footer-contents-social">
+          <a href="https://www.linkedin.com/in/john-r-deguise"><img src="linkedin.svg" alt=""/></a>
+          <a href="https://www.github.com/JdeGuise"><img src="github.svg" alt=""/></a>
+          <a href="mailto:john@johndeguise.com"><img src="email.svg" alt=""/></a>
+        </div>
 
-      <div class="footer-contents-copyright">
-        Copyright 2024 © John deGuise
+        <div class="footer-contents-copyright">
+          Copyright 2024 © John deGuise
+        </div>
       </div>
     </div>
+
   )
 }
   
