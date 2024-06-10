f77f6904292565b9cf082c331e6c0f6f113e2419
John deGuise <john.r.deguise@gmail.com>
2024-06-10 19:45:12 +0200

    Changing margin class for About section, replacing apostrophies with html char encodings

--- vite-project/src/components/About.jsx
+++ vite-project/src/components/About.jsx
@@ -1,11 +1,11 @@
 function About() {
   return (
-    <section id="about" aria-label="About me">
+    <section className="m-bot-md" id="about" aria-label="About me">
       <p>
-        In 2015, I started learning to code while I was working as a systems administrator at university. About a year later, I took my first professional step into software development as a PHP and JavaScript freelancer for a local fintech startup. After finishing a degree in computer science and entrepreneurship, I decided to enter the Salesforce ecosystem and began my career as a software consultant in Colorado. Since then, I've found opportunities to build interesting things for companies small and large - shifting back and forth between consulting and internal engineering.
+        In 2015, I started learning to code while I was working as a systems administrator at university. About a year later, I took my first professional step into software development as a PHP and JavaScript freelancer for a local fintech startup. After finishing a degree in computer science and entrepreneurship, I decided to enter the Salesforce ecosystem and began my career as a software consultant in Colorado. Since then, I&apos;ve found opportunities to build interesting things for companies small and large - shifting back and forth between consulting and internal engineering.
       </p>
       <p>
-        You know when you are using a piece of software, and something is either broken or working so badly that it makes you want to headbutt your monitor? My focus is on fixing it and helping you regain your sanity. Most recently, I've found my way to the Netherlands and I am currently focusing on building and managing high quality scheduling solutions at <a href="https://www.gomeddo.com">GoMeddo</a>.  
+        You know when you are using a piece of software, and something is either broken or working so badly that it makes you want to headbutt your monitor? My focus is on fixing it and helping you regain your sanity. Most recently, I&apos;ve found my way to the Netherlands and I am currently focusing on building and managing high quality scheduling solutions at <a href="https://www.gomeddo.com">GoMeddo</a>.  
       </p>
       <p>
         Away from my screens, I may be doing a variety of things: running, traveling, playing guitar/bass/saxophone, tinkering with 3D printing, spending time with my wife (and our dog Artie!), or just adventuring around Utrecht by bicycle. 
