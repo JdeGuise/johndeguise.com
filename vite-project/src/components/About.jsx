952bb56b604cf79aaaf74265768a4d7de9ad271a
John deGuise <john.r.deguise@gmail.com>
2024-06-10 19:13:03 +0200

    Updating About text

--- vite-project/src/components/About.jsx
+++ vite-project/src/components/About.jsx
@@ -1,16 +1,16 @@
 function About() {
   return (
-    <div>
+    <section id="about" aria-label="About me">
       <p>
-        In tegenstelling tot wat algemeen aangenomen wordt is Lorem Ipsum niet zomaar willekeurige tekst. het heeft zijn wortels in een stuk klassieke latijnse literatuur uit 45 v.Chr. en is dus meer dan 2000 jaar oud. Richard McClintock, een professor latijn aan de Hampden-Sydney College in Virginia, heeft één van de meer obscure latijnse woorden, consectetur, uit een Lorem Ipsum passage opgezocht, en heeft tijdens het zoeken naar het woord in de klassieke literatuur de onverdachte bron ontdekt.
+        In 2015, I started learning to code while I was working as a systems administrator at university. About a year later, I took my first professional step into software development as a PHP and JavaScript freelancer for a local fintech startup. After finishing a degree in computer science and entrepreneurship, I decided to enter the Salesforce ecosystem and began my career as a software consultant in Colorado. Since then, I've found opportunities to build interesting things for companies small and large - shifting back and forth between consulting and internal engineering.
       </p>
       <p>
-        Lorem Ipsum komt uit de secties 1.10.32 en 1.10.33 van "de Finibus Bonorum et Malorum" (De uitersten van goed en kwaad) door Cicero, geschreven in 45 v.Chr. Dit boek is een verhandeling over de theorie der ethiek, erg populair tijdens de renaissance. De eerste regel van Lorem Ipsum, "Lorem ipsum dolor sit amet..", komt uit een zin in sectie 1.10.32.
+        You know when you are using a piece of software, and something is either broken or working so badly that it makes you want to headbutt your monitor? My focus is on fixing it and helping you regain your sanity. Most recently, I've found my way to the Netherlands and I am currently focusing on building and managing high quality scheduling solutions at <a href="https://www.gomeddo.com">GoMeddo</a>.  
       </p>
       <p>
-        Het standaard stuk van Lorum Ipsum wat sinds de 16e eeuw wordt gebruikt is hieronder, voor wie er interesse in heeft, weergegeven. Secties 1.10.32 en 1.10.33 van "de Finibus Bonorum et Malorum" door Cicero zijn ook weergegeven in hun exacte originele vorm, vergezeld van engelse versies van de 1914 vertaling door H. Rackham.
+        Away from my screens, I may be doing a variety of things: running, traveling, playing guitar/bass/saxophone, tinkering with 3D printing, spending time with my wife (and our dog Artie!), or just adventuring around Utrecht by bicycle. 
       </p>
-    </div>
+    </section>
   )
 }
   
