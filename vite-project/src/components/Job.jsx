346533b3ba8304171902a87a96a1f75b95e0647c
John deGuise <john.r.deguise@gmail.com>
2024-06-12 16:29:09 +0200

    a tag setup markdown

--- vite-project/src/components/Job.jsx
+++ vite-project/src/components/Job.jsx
@@ -2,15 +2,17 @@ function Job(props) {
 
   if (props.job) {
     return (
-      <div className="job-content">
-        <div className="column year-range">
-          {props.job.range}
+      <a href={props.job.companyUrl} className="job-anchor" target="_blank">
+        <div className="job-content">
+            <div className="column year-range">
+              {props.job.range}
+            </div>
+            <div className="column job-info">
+              <h5>{props.job.header}</h5>
+              <p>{props.job.description}</p>
+            </div>
         </div>
-        <div className="column job-info">
-          <h5>{props.job.header}</h5>
-          <p>{props.job.description}</p>
-        </div>
-      </div>
+      </a>
     )
   }
 
