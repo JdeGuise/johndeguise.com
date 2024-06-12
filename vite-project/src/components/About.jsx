10fe95d162a5d45c6cd17955714173f012ec0fac
John deGuise <john.r.deguise@gmail.com>
2024-06-12 16:43:22 +0200

    Updating color of GoMeddo in about me section

--- vite-project/src/components/About.jsx
+++ vite-project/src/components/About.jsx
@@ -2,7 +2,7 @@ import about_json from '../data/about.json'
 
 function About() {
   return (
-    <section className="m-bot-md" id="about" aria-label="About me">
+    <section className="m-bot-md about-me" id="about" aria-label="About me">
       {about_json.about.map(function(about_paragraph) {
         return <p dangerouslySetInnerHTML={ { __html: about_paragraph } }></p>;
       })}
