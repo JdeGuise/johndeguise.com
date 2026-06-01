9acc2d7aa0a178c0299f3d0d63028d26347f418f
John deGuise <john.r.deguise@gmail.com>
2026-06-01 17:05:22 +0200

    rebrand tweaks

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import '@fontsource-variable/fraunces'
+import '@fontsource-variable/source-serif-4'
 import '@fontsource-variable/ibm-plex-sans'
 import '@fontsource/ibm-plex-mono/400.css'
 import '@fontsource/ibm-plex-mono/500.css'
